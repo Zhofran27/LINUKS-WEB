8cc9670b05b0db2490858c463bc5c38b31ff8f77
Gojikkk <gojibani28@gmail.com>
2026-06-16 01:17:02 +0700

    fix: fix laporan endpoint

--- backend/src/schemas/user/laporan.schema.ts
+++ backend/src/schemas/user/laporan.schema.ts
@@ -2,11 +2,12 @@ import { z } from 'zod';
 
 export const createLaporanSchema = z.object({
     title: z.string().min(1, 'Title is required'),
-    category_id: z.number().min(1, 'Category ID is required'),
+    category_id: z.coerce.number().int().min(1, 'Category ID is required'),
     chronology: z.string().min(1, 'Chronology is required'),
     description: z.string().min(1, 'Description is required'),
     location: z.string().min(1, 'Location is required'),
-    incident_date: z.date().min(new Date(), 'Incident date must be in the future'),
+    incident_date: z.coerce.date().min(new Date(), 'Incident date must be in the future'),
+    is_anonymous: z.coerce.number().int().min(0).max(1),
 });
 
 
