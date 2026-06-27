b6b3bd126e9c3c33a1bf5814d7f738f66055272d
Zhofran27 <zhofran275@gmail.com>
2026-06-27 13:46:28 +0700

    add file vercel for deploy

--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,9 @@
 import 'dotenv/config';
 import express, { NextFunction, Request, Response } from 'express';
 import { connectMongoDB } from './src/databases/mongodb.js';
-import { drizzle } from 'drizzle-orm/node-postgres'; 
-import  cors  from 'cors';
-import { eq } from 'drizzle-orm';
+import cors from 'cors';
 import appRoutes from './src/routes/index.js';
+
 const app = express();
 const PORT = Number(process.env.PORT) || 4000;
 
@@ -15,26 +14,26 @@ app.use(
   }),
 );
 app.use(express.json());
-
 app.use('/api', appRoutes);
 
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   console.error('Error tertangkap di Global Handler:', err);
-
   const statusCode = err.status || 500;
   const message = err.message || 'Terjadi kesalahan pada server';
-
   res.status(statusCode).json({
     error: message,
-    // (Opsional) Tampilkan stack trace hanya saat development
     stack: process.env.NODE_ENV === 'development' ? err.stack : undefined,
   });
 });
 
+// Koneksi MongoDB
 connectMongoDB();
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// Hanya listen di lokal, Vercel handle sendiri di production
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
 
 export default app;
